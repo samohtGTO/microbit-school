098f5c6d7a5479a343df1eb47c51d7f89e022e7f
samohtGTO <71775252+samohtGTO@users.noreply.github.com>
2024-03-12 09:43:46 +0100

    radio setup

--- main.ts
+++ main.ts
@@ -1,4 +1,13 @@
-input.onSound(DetectedSound.Loud, function() {
-    
-    basic.showNumber(input.soundLevel())
-})
+let radioFreq: number
+radioFreq = 0
+while (true) {
+    basic.showNumber(radioFreq)
+    if (input.buttonIsPressed(Button.B)){
+        radioFreq ++
+    } else if (input.buttonIsPressed(Button.A)) {
+        radioFreq --
+    } else if (input.logoIsPressed()) {
+        break
+    }    
+}
+radio.setGroup(radioFreq)
