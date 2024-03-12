3ec8af2e8cf5278feb2a2270ca9afec723729209
samohtGTO <71775252+samohtGTO@users.noreply.github.com>
2024-03-12 09:47:31 +0100

    radio start

--- main.ts
+++ main.ts
@@ -7,7 +7,13 @@ while (true) {
     } else if (input.buttonIsPressed(Button.A)) {
         radioFreq --
     } else if (input.logoIsPressed()) {
+        basic.clearScreen()
+        basic.pause(100)
         break
     }    
 }
+basic.showNumber(radioFreq)
+basic.clearScreen()
+basic.pause(100)
+basic.showNumber(radioFreq)
 radio.setGroup(radioFreq)
