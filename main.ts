c8deafffcdfdedf88647e2916d1116faa76f1897
samohtGTO <71775252+samohtGTO@users.noreply.github.com>
2024-03-15 14:28:09 +0100

    test

--- main.ts
+++ main.ts
@@ -20,3 +20,26 @@ basic.pause(100)
 basic.showNumber(radioFreq)
 radio.setGroup(radioFreq)
 
+
+basic.pause(100)
+PCA9685.reset(64)
+let tar:number = 0
+basic.forever(function() {
+
+    pins.digitalReadPin(DigitalPin.P0)?tar=1:tar;
+    pins.digitalReadPin(DigitalPin.P1)?tar=2:tar;
+    pins.digitalReadPin(DigitalPin.P2)?tar=3:tar;
+
+    basic.showNumber(tar)
+
+    if( tar>0){
+
+        PCA9685.setServoPosition(PCA9685.ServoNum.Servo1, 180, 64)
+        basic.pause(100)
+        tar=0
+    } else{
+        PCA9685.setServoPosition(PCA9685.ServoNum.Servo1, -180, 64)
+    }
+    
+
+})
